f84ff5badda83d539742f4ce6d9350155d9e09b9
Josh <jkkealii@gmail.com>
2017-03-09 15:33:14 +0100

    set-up basic handler/driver structure
    - `Handler.java` holds functions
    - `Driver.java` has the main function

---
+++ project_2/Driver.java
@@ -0,0 +1,28 @@
+// Driver.java
+// Tutorial for XML Parsing using DOM
+
+package xmlparsingtutorial;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+public class Driver {
+
+	public static void main(String argv[]) throws SAXException, IOException, ParserConfigurationException {
+		File xmlFile = new File("sample.xml");
+		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+		Document document = documentBuilder.parse(xmlFile);
+		document.getDocumentElement().normalize();
+		Handler.handleChannelTag(document);
+		
+	}
+	
+}
