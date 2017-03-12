47b0137d64e7019634e3fa39f59f00dcecc2a15a
Nick <markunasn@gmail.com>
2017-03-12 14:04:32 +0100

    added user prompting for XML file

--- project_2/Driver.java
+++ project_2/Driver.java
@@ -9,11 +9,21 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
+import java.util.Scanner
 public class Driver {
 
 	public static void main(String argv[]) throws SAXException, IOException, ParserConfigurationException {
-		File xmlFile = new File("sample.xml"); // needs to prompt user
-		Handler handler = new Handler();
+		Scanner input = new Scanner(System.in);
+        System.out.println("Default XML file is hamlet.xml, do you wish to use a different file? (Y/N)");
+        String choice = input.nextLine();
+        String filename = "sample.xml";
+        if(choice == 'Y' || choice == 'y'){
+            System.out.println("Enter the name of the XML file to be read: ");
+            filename = input.nextLine();
+        }
+        File xmlFile = new File(filename);
+		
+        Handler handler = new Handler();
 		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
 		Document document = documentBuilder.parse(xmlFile);
