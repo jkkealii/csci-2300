4be3acafdd39bfc48f213567d3276a86886eccd4
Josh <jkkealii@gmail.com>
2017-03-12 14:31:56 +0100

    implemented search and replace methods

--- project_2/Handler.java
+++ project_2/Handler.java
@@ -6,6 +6,12 @@ import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathFactory;
 
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NameList;
@@ -72,9 +78,88 @@ public class Handler {
         }
 	}
 
-	// public static void replaceWith (String alt) {
+	public static void replaceWith (Document doc, String fragment, String alt) {
+		String filepath = "/project_2/";
+		XPathFactory xFactory = XPathFactory.newInstance();
+        XPath xPath = xFactory.newXPath();
+        XPathExpression exp = xPath.compile("/PLAY/ACT/SCENE/SPEECH/LINE[contains(translate(text(), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), " + fragment + ")]");
 
-	// }
+        NodeList nl = (NodeList)exp.evaluate(doc.getFirstChild(), XPathConstants.NODESET);
+        for (int index = 0; index < nl.getLength(); index++) {
+            Node node = nl.item(index);
+            String original = node.getTextContent();
+
+            String regex = fragment;
+			String sourceString = node.getTextContent();
+
+			Pattern pat = Pattern.compile(regex);
+			Matcher match = pat.matcher(sourceString);
+			StringBuffer sb = new StringBuffer();
+
+			while (match.find()) {
+			    String replacement = alt;
+			    match.appendReplacement(sb, replacement);
+			}
+			match.appendTail(sb);
+			sourceString = sb.toString();
+
+			System.out.println("The sentence has been replaced as follows:");
+			System.out.println(sourceString);
+            System.out.println("Do you want to save the changes? (Y/N)");
+            // if yes:
+            System.out.println("Enter the name of the saved file (Press ENTER to overwrite)");
+            // else don't make changes
+
+            // if user makes new file
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(new File(filepath));
+			transformer.transform(source, result);
+			replaceWith(result, fragment, alt);
+            System.out.println("File saved successfully.");
+
+            // if user wants to overwrite
+            System.out.println("Do you want to overwrite the file hamlet.xml? (Y/N)");
+            node.setTextContent(sourceString);
+            System.out.println("File saved successfully.");
+            // if not, no changes
+        }
+	}
+
+	public static void replaceWith (File xml, String fragment, String alt) {
+		File xmlFile = xml;
+		Handler handler = new Handler();
+		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+		Document doc = documentBuilder.parse(xml);
+
+		XPathFactory xFactory = XPathFactory.newInstance();
+        XPath xPath = xFactory.newXPath();
+        XPathExpression exp = xPath.compile("/PLAY/ACT/SCENE/SPEECH/LINE[contains(translate(text(), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), " + fragment + ")]");
+
+        NodeList nl = (NodeList)exp.evaluate(doc.getFirstChild(), XPathConstants.NODESET);
+        for (int index = 0; index < nl.getLength(); index++) {
+            Node node = nl.item(index);
+            String original = node.getTextContent();
+
+            String regex = fragment;
+			String sourceString = node.getTextContent();
+
+			Pattern pat = Pattern.compile(regex);
+			Matcher match = pat.matcher(sourceString);
+			StringBuffer sb = new StringBuffer();
+
+			while (match.find()) {
+			    String replacement = alt;
+			    match.appendReplacement(sb, replacement);
+			}
+			match.appendTail(sb);
+			sourceString = sb.toString();
+
+            node.setTextContent(sourceString);
+        }
+	}
 
 	protected String getString (String tagName, Element root) {
         NodeList list = element.getElementsByTagName(tagName);
