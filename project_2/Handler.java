635673c6f67ffa55f212dc2257f2dd23493676f1
Josh <jkkealii@gmail.com>
2017-03-14 23:22:35 +0100

    FINISHED! let's squash bugs now
    - all functionality for the command line portion is working now
    - haven't tested too many edge cases though
    - don't mind the hamlet.xml change, it's the same

--- project_2/Handler.java
+++ project_2/Handler.java
@@ -25,73 +25,120 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.util.Scanner;
 
 public class Handler {
+	private static String filename;
 
-	public Handler () {
-		
-	}
+    public Handler (String filename) {
+        this.filename = filename;
+    }
 
-	public static int getPersonaTotal (Element root) {
-		NodeList personaList = root.getElementsByTagName("PERSONA");
-		return personaList.getLength();
-	}
+    public static int getPersonaTotal (Element root) {
+        NodeList personaList = root.getElementsByTagName("PERSONA");
+        return personaList.getLength();
+    }
 
-	public static int getActNumberOf (Node root, String character) {
-		String speaker = character;
-		NodeList speakerList = root.getChildNodes();
-		int actNumber = 0;
-		Node child;
-		for (int i = 0; i < speakerList.getLength(); i++) {
-			child = speakerList.item(i);
-			if (child instanceof Element) {
-				actNumber += getActNumberOf(child, speaker);
-			}
-			if (child.getNodeName().equals("SPEAKER") && getNodeValue(child).equals(speaker)) {
-				actNumber++;
-			}
-		}
-		return actNumber;
-	}
+    public static int getActNumberOf (Node root, String character) {
+        String speaker = character;
+        NodeList speakerList = root.getChildNodes();
+        int actNumber = 0;
+        Node child;
+        for (int i = 0; i < speakerList.getLength(); i++) {
+            child = speakerList.item(i);
+            if (child instanceof Element) {
+                actNumber += getActNumberOf(child, speaker);
+            }
+            if (child.getNodeName().equals("SPEAKER") && getNodeValue(child).equals(speaker)) {
+                actNumber++;
+            }
+        }
+        return actNumber;
+    }
 
-	public static String getNodeValue (Node node) {
-	    StringBuffer buf = new StringBuffer();
-	    NodeList children = node.getChildNodes();
-	    for (int i = 0; i < children.getLength(); i++) {
-	        Node textChild = children.item(i);
-	        if (textChild.getNodeType() != Node.TEXT_NODE) {
-	            System.err.println("Mixed content! Skipping child element " + textChild.getNodeName());
-	            continue;
-	        }
-	        buf.append(textChild.getNodeValue());
-	    }
-	    return buf.toString();
-	}
+    public static String getNodeValue (Node node) {
+        StringBuffer buf = new StringBuffer();
+        NodeList children = node.getChildNodes();
+        for (int i = 0; i < children.getLength(); i++) {
+            Node textChild = children.item(i);
+            if (textChild.getNodeType() != Node.TEXT_NODE) {
+                System.err.println("Mixed content! Skipping child element " + textChild.getNodeName());
+                continue;
+            }
+            buf.append(textChild.getNodeValue());
+        }
+        return buf.toString();
+    }
 
-	public static void searchFragment (Document doc, String fragment) throws TransformerConfigurationException, XPathExpressionException {
-		Scanner input = new Scanner(System.in);
-		double startTime,endTime,time;
+    public static void searchFragment (Document doc, String fragment) throws TransformerConfigurationException, XPathExpressionException {
+        String lowercaseFragment = fragment.toLowerCase();
+        Scanner input = new Scanner(System.in);
+        double startTime,endTime,time;
         startTime = System.currentTimeMillis();
-        String lineExpression = "/PLAY/ACT/SCENE/SPEECH/LINE[contains(translate(text(), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), '" + fragment + "')]";
+        String lineExpression = "/PLAY/ACT/SCENE/SPEECH/LINE[contains(translate(text(), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), '" + lowercaseFragment + "')]";
         try {
             XPathFactory xFactory = XPathFactory.newInstance();
             XPath xPath = xFactory.newXPath();
             XPathExpression exp = xPath.compile(lineExpression);
 
             NodeList nl = (NodeList)exp.evaluate(doc.getFirstChild(), XPathConstants.NODESET);
+            if (nl.getLength() < 1) {
+            	throw new Exception("FRAGMENT NOT FOUND");
+            }
+
             endTime = System.currentTimeMillis();
             time = (endTime - startTime)/1000;
             System.out.println("Search performed in " + time + " seconds");
             for (int index = 0; index < nl.getLength(); index++) {
-            	System.out.println("The fragment has been found in the following sentence:");
+            	if (index == 0) {
+            		System.out.println("FOUND: " + nl.getLength() + " occurrence(s) of this fragment");
+                	System.out.println("The fragment has been found in the following sentence:");           		
+            	} else {
+                	System.out.println("Another fragment has been found in the following sentence:");
+            	}
                 Node node = nl.item(index);
                 System.out.println(node.getTextContent());
             
-	            System.out.println("Do you want to replace it? (Y/N)");
-	            String answer = input.nextLine();
-	            if (answer.equals("Y") || answer.equals("y")) {
-	            	System.out.println("Enter replacement string:");
-	            	String alt = input.nextLine();
-	            	replaceWith(doc, fragment, alt);
-	            }
+                System.out.println("Do you want to replace it? (Y/N)");
+                String answer = input.nextLine();
+                if (answer.equals("Y") || answer.equals("y")) {
+                    System.out.println("Enter replacement string:");
+                    String alt = input.nextLine();
+                    replaceWith(node, fragment, alt);
+
+                    System.out.println("The sentence has been replaced as follows:");
+                    System.out.println(node.getTextContent());
+
+                    System.out.println("Do you want to save the changes? (Y/N)");
+                    String save = input.nextLine();
+
+                    if (save.equals("Y") || save.equals("y")) {
+                        System.out.println("Enter the name of the saved file (Press ENTER to overwrite)");
+                        String newName = input.nextLine();
+
+                        if (newName.equals("")) {
+                            System.out.println("Do you want to overwrite the file? (Y/N)");
+                            String overwrite = input.nextLine();
+
+                            if (overwrite.equals("Y") || overwrite.equals("y")) {
+                            	TransformerFactory transformerFactory = TransformerFactory.newInstance();
+	                            Transformer transformer = transformerFactory.newTransformer();
+	                            DOMSource source = new DOMSource(doc);
+	                            StreamResult result = new StreamResult(new File(filename));
+	                            transformer.transform(source, result);
+
+                                System.out.println("File saved successfully.");
+                            }
+                        } else {
+                            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+                            Transformer transformer = transformerFactory.newTransformer();
+                            DOMSource source = new DOMSource(doc);
+                            StreamResult result = new StreamResult(new File(newName));
+                            transformer.transform(source, result);
+
+                            replaceWith(node, alt, fragment);
+
+                            System.out.println("File saved successfully.");
+                        }
+                    }
+                }
             }
             
         } catch (Exception ex) {
@@ -100,109 +147,23 @@ public class Handler {
             Logger.getLogger(Handler.class.getName()).log(Level.SEVERE, null, ex);
             System.out.println("Sorry, fragment not found. Search performed in " + time + " seconds");
         }
-	}
+    }
 
-	public static void replaceWith (Document doc, String fragment, String alt) throws IOException, SAXException, XPathExpressionException, TransformerConfigurationException, TransformerException, ParserConfigurationException {
-		Scanner input = new Scanner(System.in);
-		XPathFactory xFactory = XPathFactory.newInstance();
-        XPath xPath = xFactory.newXPath();
-        XPathExpression exp = xPath.compile("/PLAY/ACT/SCENE/SPEECH/LINE[contains(translate(text(), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), " + fragment + ")]");
+    public static void replaceWith (Node node, String fragment, String alt) throws IOException, SAXException, XPathExpressionException, TransformerConfigurationException, TransformerException, ParserConfigurationException {
+        String nodeText = node.getTextContent();
 
-        NodeList nl = (NodeList)exp.evaluate(doc.getFirstChild(), XPathConstants.NODESET);
-        for (int index = 0; index < nl.getLength(); index++) {
-            Node node = nl.item(index);
-            String original = node.getTextContent();
+        Pattern p = Pattern.compile(fragment, Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(nodeText);
+        StringBuffer sb = new StringBuffer();
 
-            String regex = fragment;
-			String sourceString = node.getTextContent();
-
-			Pattern pat = Pattern.compile(regex);
-			Matcher match = pat.matcher(sourceString);
-			StringBuffer sb = new StringBuffer();
-
-			while (match.find()) {
-			    String replacement = alt;
-			    match.appendReplacement(sb, replacement);
-			}
-			match.appendTail(sb);
-			sourceString = sb.toString();
-
-			System.out.println("The sentence has been replaced as follows:");
-			System.out.println(sourceString);
-            System.out.println("Do you want to save the changes? (Y/N)");
-            String save = input.nextLine();
-            // if yes:
-            if (save.equals("Y") || save.equals("y")) {
-            	System.out.println("Enter the name of the saved file (Press ENTER to overwrite)");
-            	String name = input.nextLine();
-
-            	if (name.equals("")) {
-            		System.out.println("Do you want to overwrite the file? (Y/N)");
-            		String overwrite = input.nextLine();
-            		if (overwrite.equals("Y") || overwrite.equals("y")) {
-            			node.setTextContent(sourceString);
-	            		System.out.println("File saved successfully.");
-            		}
-            	} else {
-	            	TransformerFactory transformerFactory = TransformerFactory.newInstance();
-					Transformer transformer = transformerFactory.newTransformer();
-					DOMSource source = new DOMSource(doc);
-					StreamResult result = new StreamResult(new File(name));
-					transformer.transform(source, result);
-					File newFile = new File(name);
-
-					replaceWith(newFile, fragment, alt);
-
-		            System.out.println("File saved successfully.");
-            	} 
-            } 
-        }
-	}
-
-	public static void replaceWith (File xml, String fragment, String alt) throws IOException, ParserConfigurationException, SAXException, XPathExpressionException {
-		File xmlFile = xml;
-		Handler handler = new Handler();
-		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-		DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-		Document doc = documentBuilder.parse(xml);
-
-		XPathFactory xFactory = XPathFactory.newInstance();
-        XPath xPath = xFactory.newXPath();
-        XPathExpression exp = xPath.compile("/PLAY/ACT/SCENE/SPEECH/LINE[contains(translate(text(), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'), " + fragment + ")]");
-
-        NodeList nl = (NodeList)exp.evaluate(doc.getFirstChild(), XPathConstants.NODESET);
-        for (int index = 0; index < nl.getLength(); index++) {
-            Node node = nl.item(index);
-            String original = node.getTextContent();
-
-            String regex = fragment;
-			String sourceString = node.getTextContent();
-
-			Pattern pat = Pattern.compile(regex);
-			Matcher match = pat.matcher(sourceString);
-			StringBuffer sb = new StringBuffer();
-
-			while (match.find()) {
-			    String replacement = alt;
-			    match.appendReplacement(sb, replacement);
-			}
-			match.appendTail(sb);
-			sourceString = sb.toString();
-
-            node.setTextContent(sourceString);
-        }
-	}
-
-	protected static String getString (String tagName, Element root) {
-        NodeList list = root.getElementsByTagName(tagName);
-        if (list != null && list.getLength() > 0) {
-            NodeList subList = list.item(0).getChildNodes();
-
-            if (subList != null && subList.getLength() > 0) {
-                return subList.item(0).getNodeValue();
-            }
+        while (m.find()) {
+            String replacement = alt;
+            m.appendReplacement(sb, replacement);
         }
 
-        return null;
+        m.appendTail(sb);
+
+        nodeText = sb.toString();
+        node.setTextContent(nodeText);
     }
 }
